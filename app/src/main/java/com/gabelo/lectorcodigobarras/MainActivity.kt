22690abfd3facd4950856312ab9348e9164d83fc
JuanjoGabelo <juan.gabelo27217@ucaldas.edu.co>
2023-12-05 20:01:33 -0500

    Lector de Barras

---
+++ app/src/main/java/com/gabelo/lectorcodigobarras/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.gabelo.lectorcodigobarras
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.gabelo.lectorcodigobarras.databinding.ActivityMainBinding
+import com.google.zxing.integration.android.IntentIntegrator
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnScanner.setOnClickListener { initScanner() }
+    }
+    private fun initScanner() {
+        IntentIntegrator(this).initiateScan()
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Cancelado", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "El valor escaneado es: ${result.contents}", Toast.LENGTH_SHORT).show()
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+}
